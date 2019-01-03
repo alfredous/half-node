6506341ec17b05e612c7f0ae87996e425f73abd7
alfredous <alfred@familleortolani.net>
2019-01-03 19:34:57 +0100

    stage all et maintenant commit puis push

--- half-node/monNode.js
+++ half-node/monNode.js
@@ -1,1 +1,12 @@
 const http=require('http');
+const requestlistener=(request, response) => {
+  response.write('Hello from ici bas\n');
+  response.end();
+}
+
+Const server=http.createServer(requestlistener);
+const confirmationcallback =() => {
+  console.log('Server est en cours de fonctionnement');
+}
+
+server.listen(9000, confirmationcallback);
